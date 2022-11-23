16de2fc5cee9ad02a147587daff0e5ea96508ee0
6cessfuldev <6cessfuldev@gmail.com>
2022-11-23 18:16:35 +0900

    20221123

--- src/com/yuk/No7662.java
+++ src/com/yuk/No7662.java
@@ -1,270 +1,96 @@
 package com.yuk;
 
-import java.util.Scanner;
-
-class MaxQueue {
-	int maxSize;
-	int[] queArr;
-	int back;
-	
-	public MaxQueue(int maxSize) {
-		this.maxSize = maxSize;
-		this.queArr= new int[maxSize+1];
-		back = 1;
-	}
-	
-	public boolean isEmpty() {
-		if(back<=1) {
-			return true;
-		}
-		return false;
-	}
-	
-	public void push(int value) {
-		if(isFull()) return;
-		
-		int tmp = back++;
-		while(tmp>1) {
-			if(value>queArr[tmp/2]) {
-				queArr[tmp] = queArr[tmp/2];
-				tmp = tmp/2;
-			} else {
-				break;
-			}
-		}
-		
-		queArr[tmp] = value;
-	}
-	
-	public boolean isFull() {
-		if(back>=maxSize+1) {
-			return true;
-		}
-		return false;
-	}
-
-	public int poll() {
-
-		int result = queArr[1];
-
-		back--;
-		int tmp = 1;
-		while(2*tmp<back) {
-			
-			if(back>tmp*2+1) {
-				int bigger = (queArr[tmp*2] >= queArr[tmp*2+1]) ? tmp*2 : tmp*2+1;
-				if(queArr[back]>queArr[bigger]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[bigger];
-					tmp = bigger;
-				}
-			} else {
-				if(queArr[back]>queArr[tmp*2]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[tmp*2];
-					tmp = tmp*2;
-				}
-			}
-		}
-		queArr[tmp] = queArr[back];
-		
-		return result;
-	}
-	
-	public void remove(int n) {
-		if(isEmpty()) return;
-		back--;
-		int tmp = 1;
-		for (int i =back ; i > 0; i--) {
-			if(queArr[i]==n) {
-				tmp=i;
-				break;
-			}
-		}
-		if(tmp == back) return;
-		
-		while(2*tmp<back) {
-		
-			if(back>tmp*2+1) {
-				int bigger = (queArr[tmp*2] >= queArr[tmp*2+1]) ? tmp*2 : tmp*2+1;
-				if(queArr[back]>queArr[bigger]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[bigger];
-					tmp = bigger;
-				}
-			} else {
-				if(queArr[back]>queArr[tmp*2]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[tmp*2];
-					tmp = tmp*2;
-				}
-			}
-		}
-		
-		queArr[tmp] = queArr[back];
-	}
-}
-
-class MinQueue {
-	int maxSize;
-	int[] queArr;
-	int back;
-	
-	public MinQueue(int maxSize) {
-		this.maxSize = maxSize;
-		this.queArr= new int[maxSize+1];
-		back = 1;
-	}
-	
-	public boolean isEmpty() {
-		if(back<=1) {
-			return true;
-		}
-		return false;
-	}
-	
-	public void push(int value) {
-		if(isFull()) return;
-		
-		int tmp = back++;
-		while(tmp>1) {
-			if(value<queArr[tmp/2]) {
-				queArr[tmp] = queArr[tmp/2];
-				tmp = tmp/2;
-			} else {
-				break;
-			}
-		}
-		
-		queArr[tmp] = value;
-	}
-	
-	public boolean isFull() {
-		if(back>=maxSize+1) {
-			return true;
-		}
-		return false;
-	}
-
-	public int poll() {
-		int result = queArr[1];
-		
-		back--;
-		int tmp = 1;
-		while(2*tmp<back) {
-			
-			if(back>tmp*2+1) {
-				int smaller = (queArr[tmp*2] <= queArr[tmp*2+1]) ? tmp*2 : tmp*2+1;
-				if(queArr[back]<queArr[smaller]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[smaller];
-					tmp = smaller;
-				}
-			} else {
-				if(queArr[back]<queArr[tmp*2]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[tmp*2];
-					tmp = tmp*2;
-				}
-			}
-		}
-		queArr[tmp] = queArr[back];
-		
-		return result;
-	}
-	
-	public void remove(int n) {
-		if(isEmpty()) return;
-		back--;
-		
-		int tmp = 1;
-		for (int i =back ; i > 0; i--) {
-			if(queArr[i]==n) {
-				tmp=i;
-				break;
-			}
-		}
-		
-		if(tmp == back) return;
-		
-		while(2*tmp<back) {
-		
-			if(back>tmp*2+1) {
-				int smaller = (queArr[tmp*2] <= queArr[tmp*2+1]) ? tmp*2 : tmp*2+1;
-				if(queArr[back]<queArr[smaller]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[smaller];
-					tmp = smaller;
-				}
-			} else {
-				if(queArr[back]<queArr[tmp*2]) {
-					break;
-				} else {
-					queArr[tmp]=queArr[tmp*2];
-					tmp = tmp*2;
-				}
-			}
-		}
-		
-		queArr[tmp] = queArr[back];
-	}
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class No7662 {
-
-	public static void main(String[] args) {
+	
+	public static int remove(Queue<Integer> queue, Map<Integer, Integer> map) {
 		
-		Scanner scan = new Scanner(System.in);
+		int num;
+		while(true) {
+			num = queue.poll();
+			int cnt = map.getOrDefault(num,0);
+			if(cnt == 0) continue;
+			else if(cnt == 1) map.remove(num);
+			else {
+				map.put(num, cnt-1);			
+			}
+			break;
+		}
+		
+		return num;
+		
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
-		int T = scan.nextInt();
 		for (int i = 0; i < T; i++) {
+			int k = Integer.parseInt(br.readLine());
+			Map<Integer, Integer> map = new HashMap<>();
+			PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+			PriorityQueue<Integer> minQ = new PriorityQueue<>();
 			
-			int k = scan.nextInt();
-			MaxQueue maxq = new MaxQueue(k);
-			MinQueue minq = new MinQueue(k);
-			
-			for (int j = 0; j < k; j++) {
+			while(k-- >0) {				
 				
-				String op = scan.next();
-				int n = scan.nextInt();
-				if(op.equals("I")) {
-					maxq.push(n);
-					minq.push(n);
+				String[] strArr = br.readLine().split(" ");
+				
+				if(strArr[0].equals("I")) {
+					int in = Integer.parseInt(strArr[1]);
+					map.put(in, map.getOrDefault(in, 0) + 1);
+					maxQ.add(in);
+					minQ.add(in);
 				}else {
-					if(n==1) {
-						if(!maxq.isEmpty()) {
-							minq.remove(maxq.poll());					
-						}
-					}
-					else {
-						if(!maxq.isEmpty()) {
-							maxq.remove(minq.poll());
+					if(map.size()!=0) {
+						if(strArr[1].equals("1")) {
+							remove(maxQ, map);
+						} else {
+							remove(minQ, map);
 						}
 					}
 				}
+				
+//				System.out.print("map ");
+//				for (int a : map.keySet()) {
+//					System.out.print(" "+a);
+//				}
+//				System.out.println();
+//				System.out.print("max ");
+//				for (int a : maxQ) {
+//					System.out.print(" "+a);
+//				}
+//				System.out.println();
+//				System.out.print("min ");
+//				for (int a : minQ) {
+//					System.out.print(" "+a);
+//				}
 			}
 			
-			if(maxq.isEmpty()) {
+			if(map.isEmpty()) {
 				sb.append("EMPTY");
-			}else { 
-		
-				int max = maxq.queArr[1];
-				int min = minq.queArr[1];
-				
-				sb.append(max+" "+min);
-				
+			} else if(map.size()==1) {
+				int max = remove(maxQ, map);
+				sb.append(max).append(" ").append(max);
+			}else {
+				int max = remove(maxQ, map);
+				int min = remove(minQ, map);
+				sb.append(max).append(" ").append(min);
 			}
 			sb.append("\n");
 		}
 		
 		System.out.println(sb);
 	}
+
 }
